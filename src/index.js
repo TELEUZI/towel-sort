4eb21ed6e499820e00a3e90a8039c367c3bc8fbb
TELEUZI <41520794+TELEUZI@users.noreply.github.com>
2021-01-31 21:46:36 +0300

    feat: towelSort function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function towelSort(matrix) {
-    return [];
-}
+const towelSort = matrix => Array.isArray(matrix) ?
+    matrix.reduce((sortedArr, currentArr, currentIndex) =>
+        sortedArr.concat(isOdd(currentIndex) ? currentArr : currentArr.reverse()), []) : [];
+
+const isOdd = number => !(number % 2);
+
+module.exports = towelSort;
